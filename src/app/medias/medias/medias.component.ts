14b701cd47e2e96472a81cf85f24bcc539d8558e
Olivier <leplus.olivier@gmail.com>
2018-04-27 13:09:29 +0200

    feat(animations): Add enter/leave animations on medias

--- src/app/medias/medias/medias.component.ts
+++ src/app/medias/medias/medias.component.ts
@@ -2,10 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../../shared/config.service';
 import { ApiService } from '../../shared/api.service';
 
+import {trigger, stagger, animate, style, group, query as q, transition, keyframes} from '@angular/animations';
+const query = (s,a,o={optional:true})=>q(s,a,o);
+
+export const mediaTransition = trigger('mediaTransition',
+[
+  transition(':enter', [
+    query('.player', style({ opacity: 0 })),
+    query('.player', stagger(100, [
+      style({ transform: 'translateY(100px)' }),
+      animate('.3s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(0px)', opacity: 1})),
+    ])),
+  ]),
+  transition(':leave', [
+    query('.players', stagger(300, [
+      style({ transform: 'translateY(0px)', opacity: 1 }),
+      animate('.3s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(100px)', opacity: 0})),
+    ])),        
+  ])
+]);
+
 @Component({
   selector: 'app-medias',
   templateUrl: './medias.component.html',
-  styleUrls: ['./medias.component.css']
+  styleUrls: ['./medias.component.css'],
+  animations: [ mediaTransition ],
+  host: {
+    '[@mediaTransition]': '',
+  }
 })
 export class MediasComponent implements OnInit {
 
