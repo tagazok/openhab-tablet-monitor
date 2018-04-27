3e7af7a34cbe296a7bbf52e1595a44202a70068a
Olivier <leplus.olivier@gmail.com>
2018-04-27 13:27:12 +0200

    feat(animations): Add animations on weather page

--- src/app/weather/weather/weather.component.ts
+++ src/app/weather/weather/weather.component.ts
@@ -1,10 +1,56 @@
 import { Component, OnInit } from "@angular/core";
 import { environment } from "../../../environments/environment";
+import {trigger, stagger, animate, style, group, query as q, transition, keyframes} from '@angular/animations';
+const query = (s,a,o={optional:true})=>q(s,a,o);
+
+export const weatherTransition = trigger('weatherTransition', [
+  transition(':enter', [
+    group([
+      query('.area-center', style({ opacity: 0 })),
+      query('.area-top', style({ opacity: 0 })),
+      query('.area-bottom', style({ opacity: 0 })),
+    ]),
+    group([
+      query('.area-center', stagger(100, [
+        style({ transform: 'scale(.7)' }),
+        animate('.3s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'scale(1)', opacity: 1}))
+      ])),
+      query('.area-top', stagger(0, [
+        style({ transform: 'translateY(-100px)' }),
+        animate('.3s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(0px)', opacity: 1})),
+      ])),
+      query('.area-bottom', stagger(0, [
+        style({ transform: 'translateY(100px)' }),
+        animate('.3s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(0px)', opacity: 1})),
+      ])),
+    ])
+  ]),
+  transition(':leave', [
+    group([
+      query('.area-center', stagger(100, [
+        style({ transform: 'scale(1)' }),
+        animate('.3s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'scale(.7)', opacity: 0})),
+      ])),
+      query('.area-top', stagger(0, [
+        style({ transform: 'translateY(0px)' }),
+        animate('.3s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(-100px)', opacity: 0})),
+      ])),
+      query('.area-bottom', stagger(0, [
+        style({ transform: 'translateY(0px)' }),
+        animate('.3s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(100px)', opacity: 0})),
+      ])),
+    ])
+  ])
+]);
 
 @Component({
   selector: "app-weather",
   templateUrl: "./weather.component.html",
-  styleUrls: ["./weather.component.css"]
+  styleUrls: ["./weather.component.css"],
+  animations: [ weatherTransition ],
+  host: {
+    '[@weatherTransition]': ''
+  }
 })
 export class WeatherComponent implements OnInit {
   forecast: any;
@@ -43,7 +89,9 @@ export class WeatherComponent implements OnInit {
   }
 
   getNextDays() {
-    return this.forecast.list.slice(1, 6);
+    if (this.forecast)
+      return this.forecast.list.slice(1, 6);
+    return [];
   }
 
   getIcon(code) {
